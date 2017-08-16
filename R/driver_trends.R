f7e2254d284255bf335847782317a64660165aa3
Oliver <soliver@usgs.gov>
2017-08-16 16:20:33 -0500

    added annual calculation

--- R/driver_trends.R
+++ R/driver_trends.R
@@ -39,6 +39,7 @@ for (i in 2:length(cutoffs)) {
     mutate(month = month(time)) %>%
     mutate(year = year(time))
   
+  # edit some of these calculations - as some don't make sense (e.g., mins and maxes may not be useful, but maybe for air temp?)
   jas <- temp.dat %>% 
     filter(month %in% 7:9) %>%
     group_by(site_id, year) %>%
@@ -79,6 +80,16 @@ for (i in 2:length(cutoffs)) {
               ShortWave_mean = slope(year, ShortWave_mean), LongWave_mean = slope(year, LongWave_mean), AirTemp_mean = slope(year, AirTemp_mean), RelHum_mean = slope(year, RelHum_mean), WindSpeed_mean = slope(year, WindSpeed_mean)) %>%
     mutate(scale = 'amj')
   
+  annual <- temp.dat %>% 
+    group_by(site_id, year) %>%
+    select(-c(time, month)) %>%
+    summarise_all(funs(min = min, max = max, mean = mean)) %>%
+    summarise(ShortWave_min = slope(year, ShortWave_min), LongWave_min = slope(year, LongWave_min), AirTemp_min = slope(year, AirTemp_min), RelHum_min = slope(year, RelHum_min), WindSpeed_min = slope(year, WindSpeed_min),
+              ShortWave_max = slope(year, ShortWave_max), LongWave_max = slope(year, LongWave_max), AirTemp_max = slope(year, AirTemp_max), RelHum_max = slope(year, RelHum_max), WindSpeed_max = slope(year, WindSpeed_max),
+              ShortWave_mean = slope(year, ShortWave_mean), LongWave_mean = slope(year, LongWave_mean), AirTemp_mean = slope(year, AirTemp_mean), RelHum_mean = slope(year, RelHum_mean), WindSpeed_mean = slope(year, WindSpeed_mean)) %>%
+    mutate(scale = 'annual')
+  
+  trends[[i-1]] <- rbind(jas, ond, jfm, amj, annual)
   
 }
 myfiles <- lapply(files, readr::read_csv)
