938fde6da3be94630c92c304459e0a12886a5ad2
Oliver <soliver@usgs.gov>
2017-08-18 16:38:09 -0500

    updated maps to feature more continuous color scheme. Also made it colorblind-friendly

--- R/map_trends.R
+++ R/map_trends.R
@@ -3,6 +3,8 @@ library(ggplot2)
 library(mapproj)
 library(ggthemes)
 library(cowplot)
+library(leaflet)
+
 source('R/maps.R')
 trends <- readr::read_csv('cached_data/driver_trends.csv')
 trends <- unique(trends)
@@ -58,94 +60,84 @@ plot.trends <- function(map.dat, var, var.legend, season, round.val = 0) {
     labs(title = season)
 }
 
+get.cols <- function(var){
+bins = pretty(quantile(var, probs = c(0.01, 0.99), na.rm = T), 100)
+key.bins = pretty(quantile(var, probs = c(0.01,0.99), na.rm = T), 5, min.n = 4)
+pal = colorRampPalette(rev(brewer.pal(9, 'RdYlBu')))(length(bins))
+bin.cols = bins[findInterval(var, bins, all.inside = TRUE)]
+return(list(bin.cols, key.bins, pal))
+}
+
+plot.trends.cont <- function(map.dat, var, var.legend, season, col.bins) {
+  ggplot() +
+    geom_polygon(data = study.site, aes(long, lat, group = group),fill = 'gray', color = 'white') +
+    geom_polygon(data = map.dat, aes_string('long', 'lat', group = 'group', fill = factor(col.bins[[1]]), color = factor(col.bins[[1]]))) +
+    scale_fill_manual(values = col.bins[[3]], name = var.legend, breaks = col.bins[[2]]) + 
+    #scale_fill_brewer(type = 'div', palette = 'Spectral', direction = -1, name = var.legend) +
+    #scale_color_brewer(type = 'div', palette = 'Spectral', direction = -1, guide = F) +
+    scale_color_manual(values = col.bins[[3]], guide = F) + 
+    coord_map('albers', lat0 = 41, lat1 = 50) +
+    #facet_wrap(~timescale, scales = 'fixed', nrow = 2, ncol = 2)
+    theme_map() +
+    theme(legend.position = c(0, 0), 
+          legend.justification = c(0, 0),
+          legend.direction = 'horizontal',
+          legend.key = element_rect(fill = "transparent", colour = "transparent")) +
+    guides(fill = guide_legend(title.position = 'top', title.hjust = 0.5, label.position = 'bottom', nrow = 1)) +
+    labs(title = season)
+}
+
+plot.trends.cont(annual.map, 'ShortWave_mean', 'Shortwave Decadal Trend', 'annual', col.bins = get.cols(annual.map$ShortWave_mean))
+
 png('figures/shortwave_byseason.png',  res = res, height = fig.h, width = fig.w, units = 'in')
-plot_grid(plot.trends(jfm.map, 'ShortWave_mean', 'Shortwave Decadal Trend', 'JFM'),
-plot.trends(amj.map, 'ShortWave_mean', 'Shortwave Decadal Trend', 'AMJ'),
-plot.trends(jas.map, 'ShortWave_mean', 'Shortwave Decadal Trend', 'JAS'),
-plot.trends(ond.map, 'ShortWave_mean', 'Shortwave Decadal Trend', 'OND'), nrow = 2)
-dev.off()
-
-
-bins <- seq(-0.4, 1.6, by = 0.2)
-jfm.map$AirTemp_mean_bins <- bins[findInterval(jfm.map$AirTemp_mean, bins)]
-amj.map$AirTemp_mean_bins <- bins[findInterval(amj.map$AirTemp_mean, bins)]
-jas.map$AirTemp_mean_bins <- bins[findInterval(jas.map$AirTemp_mean, bins)]
-ond.map$AirTemp_mean_bins <- bins[findInterval(ond.map$AirTemp_mean, bins)]
-
-# make seasonal plots of all vars
-
-png('figures/airtemp_byseason.png',  res = res, height = fig.h, width = fig.w, units = 'in')
-plot_grid(plot.trends(jfm.map, 'AirTemp_mean_bins', 'Air Temp Decadal Trend', 'JFM', 1),
-          plot.trends(amj.map, 'AirTemp_mean_bins', 'Air Temp Decadal Trend', 'AMJ', 1),
-          plot.trends(jas.map, 'AirTemp_mean_bins', 'Air Temp Decadal Trend', 'JAS', 1),
-          plot.trends(ond.map, 'AirTemp_mean_bins', 'Air Temp Decadal Trend', 'OND', 1), nrow = 2)
+plot_grid(plot.trends.cont(jfm.map, 'ShortWave_mean', 'Shortwave Decadal Trend', 'JFM', col.bins = get.cols(jfm.map$ShortWave_mean)),
+plot.trends.cont(amj.map, 'ShortWave_mean', 'Shortwave Decadal Trend', 'AMJ', col.bins = get.cols(amj.map$ShortWave_mean)),
+plot.trends.cont(jas.map, 'ShortWave_mean', 'Shortwave Decadal Trend', 'JAS', col.bins = get.cols(jas.map$ShortWave_mean)),
+plot.trends.cont(ond.map, 'ShortWave_mean', 'Shortwave Decadal Trend', 'OND',  col.bins = get.cols(ond.map$ShortWave_mean)), nrow = 2)
 dev.off()
 
 png('figures/longwave_byseason.png',  res = res, height = fig.h, width = fig.w, units = 'in')
-plot_grid(plot.trends(jfm.map, 'LongWave_mean', 'Longwave Decadal Trend', 'JFM'),
-          plot.trends(amj.map, 'LongWave_mean', 'Longwave Decadal Trend', 'AMJ'),
-          plot.trends(jas.map, 'LongWave_mean', 'Longwave Decadal Trend', 'JAS'),
-          plot.trends(ond.map, 'LongWave_mean', 'Longwave Decadal Trend', 'OND'), nrow = 2)
+plot_grid(plot.trends.cont(jfm.map, 'LongWave_mean', 'Longwave Decadal Trend', 'JFM', col.bins = get.cols(jfm.map$LongWave_mean)),
+          plot.trends.cont(amj.map, 'LongWave_mean', 'Longwave Decadal Trend', 'AMJ', col.bins = get.cols(amj.map$LongWave_mean)),
+          plot.trends.cont(jas.map, 'LongWave_mean', 'Longwave Decadal Trend', 'JAS', col.bins = get.cols(jas.map$LongWave_mean)),
+          plot.trends.cont(ond.map, 'LongWave_mean', 'Longwave Decadal Trend', 'OND',  col.bins = get.cols(ond.map$LongWave_mean)), nrow = 2)
 dev.off()
 
+
 png('figures/relhumidity_byseason.png',  res = res, height = fig.h, width = fig.w, units = 'in')
-plot_grid(plot.trends(jfm.map, 'RelHum_mean', 'Rel Humidity Decadal Trend', 'JFM'),
-          plot.trends(amj.map, 'RelHum_mean', 'Rel Humidity Decadal Trend', 'AMJ'),
-          plot.trends(jas.map, 'RelHum_mean', 'Rel Humidity Decadal Trend', 'JAS'),
-          plot.trends(ond.map, 'RelHum_mean', 'Rel Humidity Decadal Trend', 'OND'), nrow = 2)
+plot_grid(plot.trends.cont(jfm.map, 'RelHum_mean', 'Rel Humidity Decadal Trend', 'JFM 1980-2015', col.bins = get.cols(jfm.map$RelHum_mean)),
+          plot.trends.cont(amj.map, 'RelHum_mean', 'Rel Humidity Decadal Trend', 'AMJ 1980-2015', col.bins = get.cols(amj.map$RelHum_mean)),
+          plot.trends.cont(jas.map, 'RelHum_mean', 'Rel Humidity Decadal Trend', 'JAS 1980-2015', col.bins = get.cols(jas.map$RelHum_mean)),
+          plot.trends.cont(ond.map, 'RelHum_mean', 'Rel Humidity Decadal Trend', 'OND 1980-2015',  col.bins = get.cols(ond.map$RelHum_mean)), nrow = 2)
 dev.off()
 
 png('figures/windspeed_byseason.png',  res = res, height = fig.h, width = fig.w, units = 'in')
-plot_grid(plot.trends(jfm.map, 'WindSpeed_mean', 'Windspeed Decadal Trend', 'JFM', 1),
-          plot.trends(amj.map, 'WindSpeed_mean', 'Windspeed Decadal Trend', 'AMJ', 1),
-          plot.trends(jas.map, 'WindSpeed_mean', 'Windspeed Decadal Trend', 'JAS', 1),
-          plot.trends(ond.map, 'WindSpeed_mean', 'Windspeed Decadal Trend', 'OND', 1), nrow = 2)
+plot_grid(plot.trends.cont(jfm.map, 'WindSpeed_mean', 'Windspeed Decadal Trend', 'JFM 1980-2015', col.bins = get.cols(jfm.map$WindSpeed_mean)),
+          plot.trends.cont(amj.map, 'WindSpeed_mean', 'Windspeed Decadal Trend', 'AMJ 1980-2015', col.bins = get.cols(amj.map$WindSpeed_mean)),
+          plot.trends.cont(jas.map, 'WindSpeed_mean', 'Windspeed Decadal Trend', 'JAS 1980-2015', col.bins = get.cols(jas.map$WindSpeed_mean)),
+          plot.trends.cont(ond.map, 'WindSpeed_mean', 'Windspeed Decadal Trend', 'OND 1980-2015',  col.bins = get.cols(ond.map$WindSpeed_mean)), nrow = 2)
 dev.off()
 
-png('figures/drivervars_annual.png', res = res, height = fig.h, width = fig.w, units = 'in')
-plot_grid(plot.trends(annual.map, 'ShortWave_mean', 'Shortwave Decadal Trend', 'annual', 0), 
-          plot.trends(annual.map, 'LongWave_mean', 'Longwave Decadal Trends', 'annual', 0), 
-          plot.trends(annual.map, 'RelHum_mean', 'Rel Humidity Decadal Trend', 'annual', 0),
-          plot.trends(annual.map, 'WindSpeed_mean', 'Windspeed Decadal Trend', 'annual', 1), nrow = 2)
-dev.off()
-
-
-
-
-
-lakes.df$col <- as.factor(as.character())
-png('figures/ShortWave_trends.png', res = res, height = fig.h, width = fig.w, units = 'in')
-par(mfrow=c(2,2))
-ggplot() +
-  geom_polygon(data = study.site, aes(long, lat, group = group),fill = 'darkgray', color = 'white') +
-  geom_polygon(data = jas.map, aes(long, lat, group = group, fill = factor(round(ShortWave_mean, 0)), color = factor(round(ShortWave_mean, 0)))) +
-  scale_fill_brewer(type = 'div', palette = 'Spectral', direction = -1, name = 'Shortwave Decadal Trend') +
-  scale_color_brewer(type = 'div', palette = 'Spectral', direction = -1, guide = F) +
-  coord_map('albers', lat0 = 41, lat1 = 50) +
-  #facet_wrap(~timescale, scales = 'fixed', nrow = 2, ncol = 2)
-  theme_map() +
-  theme(legend.position = c(1, 1), 
-        legend.justification = c(1, 1),
-        legend.direction = 'horizontal') +
-  guides(fill = guide_legend(title.position = 'top', title.hjust = 0.5, label.position = 'bottom', nrow = 1))
+png('figures/airtemp_byseason.png',  res = res, height = fig.h, width = fig.w, units = 'in')
+plot_grid(plot.trends.cont(jfm.map, 'AirTemp_mean', 'Air Temp Decadal Trend', 'JFM 1980-2015', col.bins = get.cols(jfm.map$AirTemp_mean)),
+          plot.trends.cont(amj.map, 'AirTemp_mean', 'Air Temp Decadal Trend', 'AMJ 1980-2015', col.bins = get.cols(amj.map$AirTemp_mean)),
+          plot.trends.cont(jas.map, 'AirTemp_mean', 'Air Temp Decadal Trend', 'JAS 1980-2015', col.bins = get.cols(jas.map$AirTemp_mean)),
+          plot.trends.cont(ond.map, 'AirTemp_mean', 'Air Temp Decadal Trend', 'OND 1980-2015',  col.bins = get.cols(ond.map$AirTemp_mean)), nrow = 2)
 dev.off()
 
 
-############
-# plot trends function
+png('figures/drivervars_annual.png',  res = res, height = fig.h, width = fig.w, units = 'in')
+plot_grid(plot.trends.cont(annual.map, 'ShortWave_mean', 'Shortwave Decadal Trend', 'JFM 1980-2015', col.bins = get.cols(annual.map$ShortWave_mean)),
+          plot.trends.cont(annual.map, 'LongWave_mean', 'Longwave Decadal Trend', 'AMJ 1980-2015', col.bins = get.cols(annual.map$LongWave_mean)),
+          plot.trends.cont(annual.map, 'RelHum_mean', 'Rel Humidity Decadal Trend', 'JAS 1980-2015', col.bins = get.cols(annual.map$RelHum_mean)),
+          plot.trends.cont(annual.map, 'WindSpeed_mean', 'Windspeed Decadal Trend', 'OND 1980-2015',  col.bins = get.cols(annual.map$WindSpeed_mean)), nrow = 2)
+dev.off()
+
+png('figures/airtemp_annual.png', res = res, height = fig.h, width = fig.w, units = 'in')
+plot.trends.cont(annual.map, 'AirTemp_mean_bins', 'Air Temp Decadal Trend', 'Annual 1980-2015', col.bins = get.cols(annual.map$AirTemp_mean))
+dev.off()
 
 
 
 
-m2 <- m1 +
-  ggplot(lakes.df) +
-  aes(long, lat, group = group, fill = ShortWave_mean) +
-  geom_polygon() +
-  coord_map() +
 
-########## non ggplot answer
-study.states <- c('minnesota', 'michigan', 'wisconsin')
-
-study.site <- map('state', regions = study.states)
-plot(x = jas.map$long, y = jas.map$lat, col = jas.map$ShortWave_mean)
-study.site <- subset(study.site, names %in% c('minnesota', 'michigan', 'wisconsin'))
