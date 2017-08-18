3870238147fa9875319805eb28857efc5b7cb091
Oliver <soliver@usgs.gov>
2017-08-18 13:15:46 -0500

    add write lake trends csv

--- R/lake_trends.R
+++ R/lake_trends.R
@@ -1,3 +1,6 @@
+detach('package:plyr', unload = TRUE)
+library(dplyr)
+
 lake.dat <- readr::read_tsv('NLDAS_thermal_metrics.tsv')
 
 slope = function(x,y){
@@ -6,5 +9,8 @@ slope = function(x,y){
 
 lake.trends <- lake.dat %>%
   select(c(year, site_id, mean_surf_jas)) %>%
+  filter(year %in% 1980:2015) %>%
   group_by(site_id) %>%
   summarise(mean_surf_jas_slope = slope(year, mean_surf_jas))
+setwd("~/Spatial-Trends")
+write.csv(lake.trends, 'cached_data/lake_trends.csv', row.names = F)
